740466810afea61121b1b600c570c6fb2c55815b
DRobinson <drobinson@benchmarkeducation.com>
2024-08-04 11:23:56 -0400

    add generate podcast

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,19 +13,19 @@ export default function RootLayout({
       <main className="relative flex bg-black-3">
         <LeftSidebar />
 
-        <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14 ">
-          <div className="mx-auto flex w-full max-w-5xl">
-            <div className="flex h-16 items-center justify-center md:hidden">
+        <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">
               <Image
                 src="/icons/logo.svg"
-                alt="menu icon"
                 width={30}
                 height={30}
+                alt="menu icon"
               />
               <MobileNav />
             </div>
             <div className="flex flex-col md:pb-14">
-              Toaster (notification popups)
+              TOASTER
               {children}
             </div>
           </div>
