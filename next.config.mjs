1d308edc705ba9f32fe69081fdafae55eca934bd
DRobinson <drobinson@benchmarkeducation.com>
2024-08-07 10:53:48 -0400

    addpodcast details page

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "reminiscent-armadillo-416.convex.cloud",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
     ],
   },
 };
