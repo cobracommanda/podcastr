49266b7331b4a2b58ddb93025943d1d510a193e3
DRobinson <drobinson@benchmarkeducation.com>
2024-08-07 20:06:05 -0400

    add podcast player

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
-import ConvexClerkProvider from "./providers/ConvexClerkProvider";
+import ConvexClerkProvider from "@/providers/ConvexClerkProvider";
+import AudioProvider from "@/providers/AudioProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+const manrope = Manrope({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Podcastr",
@@ -20,9 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ConvexClerkProvider> {children}</ConvexClerkProvider>
-      </body>
+      <ConvexClerkProvider>
+        <AudioProvider>
+          <body className={manrope.className}>{children}</body>
+        </AudioProvider>
+      </ConvexClerkProvider>
     </html>
   );
 }
